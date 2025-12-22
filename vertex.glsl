65089d2b1eb96f40e843023f0a8a5fd31f68fb73
Pesi <arthur.pesi@gmail.com>
2025-12-22 20:59:10 -0300

    triangulo flutuante

--- vertex.glsl
+++ vertex.glsl
@@ -1,4 +1,12 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec3 aColor;
+uniform vec2 player;
+uniform vec2 floating;
+out vec3 ourColor;
+
 void main()
-{ gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0); }
+{ 
+    gl_Position = vec4(aPos.x - player.x, floating.y + aPos.y - player.y, 0.0, 1.0); 
+    ourColor = gl_Position.xyz;
+}
